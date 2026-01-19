3991d5c666dc0ee52cf3e86177ce426e7d16b944
nnatttaly <dmitrievan730@gmail.com>
2026-01-19 21:41:29 +0500

    Обучениеееей

--- src/consts/paths.ts
+++ src/consts/paths.ts
@@ -1,3 +1,5 @@
 export const SCORE_ICON_PATH = "src/assets/icons/flame.svg";
 export const MOVES_ICON_PATH = "src/assets/icons/arrows.svg";
 export const CUP_ICON_PATH = "src/assets/icons/cup.svg";
+export const HERO_ICON_PATH = "src/assets/images/tutorial/hero.svg";
+export const DIALOG_BUBBLE_ICON_PATH = "src/assets/images/tutorial/dialog-bubble.svg";
