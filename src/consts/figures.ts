b33221165a52936abb66ec64d82cf96f830d8ead
MrPlesk2 <plesk9@mail.ru>
2026-01-21 12:47:57 +0500

    try 3

--- src/consts/figures.ts
+++ src/consts/figures.ts
@@ -1,28 +1,52 @@
 import { Figure } from "types";
+import pencilImg from "@/assets/images/figures/pencil.svg";
+import questionBookImg from "@/assets/images/figures/questionBook.svg";
+import openBookImg from "@/assets/images/figures/openBook.svg";
+import briefcaseImg from "@/assets/images/figures/briefcase.svg";
+import bonnetImg from "@/assets/images/figures/bonnet.svg";
+import roundMessageImg from "@/assets/images/figures/roundMessage.svg";
+import rectangleMessageImg from "@/assets/images/figures/rectangleMessage.svg";
+import letterImg from "@/assets/images/figures/letter.svg";
+import phoneImg from "@/assets/images/figures/phone.svg";
+import smartphoneImg from "@/assets/images/figures/smartphone.svg";
+import goldenCellImg from "@/assets/images/figures/goldenCell.svg";
+import starImg from "@/assets/images/figures/star.svg";
+import diamondImg from "@/assets/images/figures/diamond.svg";
+import teamCellImg from "@/assets/images/figures/happyTeam.svg";
+import teamImage0Img from "@/assets/images/figures/teamHappy0.svg";
+import teamImage1Img from "@/assets/images/figures/teamHappy1.svg";
+import teamImage2Img from "@/assets/images/figures/teamHappy2.svg";
+import teamImage3Img from "@/assets/images/figures/teamHappy3.svg";
+import teamImg from "@/assets/images/figures/happyTeam.svg";
+import questionImg from "@/assets/images/figures/question.svg";
+import heartImg from "@/assets/images/figures/heart.svg";
+import handshakeImg from "@/assets/images/figures/handshake.svg";
+import kpiImg from "@/assets/images/figures/kpi.svg";
+import bulbImg from "@/assets/images/figures/bulb.svg";
 
 export const FIGURE_PATHS: Record<Figure, string> = {
-  pencil: "src/assets/images/figures/pencil.svg",
-  questionBook: "src/assets/images/figures/questionBook.svg",
-  openBook: "src/assets/images/figures/openBook.svg",
-  briefcase: "src/assets/images/figures/briefcase.svg",
-  bonnet: "src/assets/images/figures/bonnet.svg",
-  roundMessage: "src/assets/images/figures/roundMessage.svg",
-  rectangleMessage: "src/assets/images/figures/rectangleMessage.svg",
-  letter: "src/assets/images/figures/letter.svg",
-  phone: "src/assets/images/figures/phone.svg",
-  smartphone: "src/assets/images/figures/smartphone.svg",
-  goldenCell: "src/assets/images/figures/goldenCell.svg",
-  star: "src/assets/images/figures/star.svg",
-  diamond: "src/assets/images/figures/diamond.svg",
-  teamCell: "src/assets/images/figures/happyTeam.svg",
-  teamImage0: "src/assets/images/figures/teamHappy0.svg",
-  teamImage1: "src/assets/images/figures/teamHappy1.svg",
-  teamImage2: "src/assets/images/figures/teamHappy2.svg",
-  teamImage3: "src/assets/images/figures/teamHappy3.svg",
-  team: "src/assets/images/figures/happyTeam.svg",
-  question: "src/assets/images/figures/question.svg",
-  heart: "src/assets/images/figures/heart.svg",
-  handshake: "src/assets/images/figures/handshake.svg",
-  kpi: "src/assets/images/figures/kpi.svg",
-  bulb: "src/assets/images/figures/bulb.svg",
+  pencil: pencilImg,
+  questionBook: questionBookImg,
+  openBook: openBookImg,
+  briefcase: briefcaseImg,
+  bonnet: bonnetImg,
+  roundMessage: roundMessageImg,
+  rectangleMessage: rectangleMessageImg,
+  letter: letterImg,
+  phone: phoneImg,
+  smartphone: smartphoneImg,
+  goldenCell: goldenCellImg,
+  star: starImg,
+  diamond: diamondImg,
+  teamCell: teamCellImg,
+  teamImage0: teamImage0Img,
+  teamImage1: teamImage1Img,
+  teamImage2: teamImage2Img,
+  teamImage3: teamImage3Img,
+  team: teamImg,
+  question: questionImg,
+  heart: heartImg,
+  handshake: handshakeImg,
+  kpi: kpiImg,
+  bulb: bulbImg,
 };
